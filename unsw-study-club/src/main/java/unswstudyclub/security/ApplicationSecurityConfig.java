5c9baf542ec2699cbe24aedff6570a27b151ac85
KatrinaaDing <xiaoding1998@hotmail.com>
2020-05-22 23:49:24 +0800

    role-base and permission-base authorization

--- unsw-study-club/src/main/java/unswstudyclub/security/ApplicationSecurityConfig.java
+++ unsw-study-club/src/main/java/unswstudyclub/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package unswstudyclub.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,18 +9,36 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static unswstudyclub.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+
+                // order of matchers matter
+
+                //.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission()) // authorization only on DELETE with specific api
+                //.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                //.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                //.antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRANIEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -31,12 +50,29 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails kat = User.builder()
                 .username("katrina")
-                .password("xiaoding")
-                .roles("STUDENT")
+                .password(passwordEncoder.encode("pw123"))
+//                .roles(ApplicationUserRole.STUDENT.name())    // authority on roles
+                .authorities(STUDENT.getGrantedAuthorities())   // authority on permission
+                .build();
+
+        UserDetails linda = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("pw123"))
+//                .roles(ApplicationUserRole.ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())   // authority on permission
+                .build();
+
+        UserDetails tom = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("pw123"))
+//                .roles(ApplicationUserRole.ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())   // authority on permission
                 .build();
 
         return new InMemoryUserDetailsManager(
-                kat
+                kat,
+                linda,
+                tom
         );
     }
 }
---
+++ unsw-study-club/src/main/java/unswstudyclub/security/ApplicationUserRole.java
@@ -0,0 +1,36 @@
+package unswstudyclub.security;
+
+import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static unswstudyclub.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permisions) {
+        this.permissions = permisions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))  // map each permisision to SimpleGrantedAuthority
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name())); // add role as SimpleGrantedAuthority
+        return permissions;
+    }
+}
